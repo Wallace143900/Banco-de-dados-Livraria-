2c18cccd289e00e385b3fcda7bc77c764d9a977d
Wallace143900 <hiperguardi@gmail.com>
2024-05-24 13:07:53 -0300

    tabela das informações de contato criado

--- sql/database.sql
+++ sql/database.sql
@@ -5,3 +5,13 @@ CREATE TABLE authors (
     "created_at" TIMESTAMP DEFAULT NOW(),
     "updated_at" TIMESTAMP
 );
+
+CREATE TABLE contact_infos (
+    "id" SERIAL PRIMARY KEY,
+    "phone" VARCHAR(20),
+    "email" VARCHAR(100),
+    "author_id" INTEGER,
+    FOREIGN KEY (author_id) REFERENCES authors(id) ON DELETE CASCADE,
+    "created_at" TIMESTAMP DEFAULT NOW(),
+    "updated_at" TIMESTAMP
+);
