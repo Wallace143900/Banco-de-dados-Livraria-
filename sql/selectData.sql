f75125af0f29c79e09678aac5dfc2c89f889834c
Wallace143900 <hiperguardi@gmail.com>
2024-05-24 13:17:44 -0300

    Esta consulta retorna informações detalhadas sobre o livro "Harry Potter"

--- sql/selectData.sql
+++ sql/selectData.sql
@@ -10,3 +10,8 @@ SELECT books.id AS book_id, books.name AS book_name, categories.id AS category_i
 FROM books
 JOIN books_categories ON books.id = books_categories.book_id
 JOIN categories ON books_categories.category_id = categories.id;
+
+SELECT books.id AS book_id, books.name AS book_name, authors.id AS author_id, authors.name AS author_name, authors.bio AS author_bio
+FROM books
+JOIN authors ON books.author_id = authors.id
+WHERE books.name = 'Harry Potter';
