221f6fc32fa9e592c414d39247c0231377a5ed39
Wallace143900 <hiperguardi@gmail.com>
2024-05-24 13:10:08 -0300

    inseri dados dos autores

---
+++ sql/insertData.sql
@@ -0,0 +1,4 @@
+INSERT INTO authors (name, bio) VALUES
+('Eiichiro Oda', 'Eiichiro Oda é um mangaká conhecido pela criação do mangá One Piece.'),
+('J. K. Rowling', 'J. K. Rowling é uma escritora, roteirista e produtora cinematográfica britânica, notória por escrever a série de livros Harry Potter.'),
+('Osvaldo Silva', 'Autor e compositor brasileiro.') RETURNING *;
