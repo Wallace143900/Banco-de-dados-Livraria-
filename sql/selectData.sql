5f6d1eb59869d6a1506f22fdddf4068bdfb8c2cb
Wallace143900 <hiperguardi@gmail.com>
2024-05-24 13:15:24 -0300

    selecionei registro da tabela books
    pertencente à categoria "Fantasia". Para isso, ela junta as tabelas books, books_categories e categories, usando as chaves estrangeiras book_id e category_id.

--- sql/selectData.sql
+++ sql/selectData.sql
@@ -1,1 +1,7 @@
 SELECT * FROM books;
+
+SELECT books.*
+FROM books
+JOIN books_categories ON books.id = books_categories.book_id
+JOIN categories ON books_categories.category_id = categories.id
+WHERE categories.name = 'Fantasia';
