f699dc059256157946cfdfcf6fdce9ac68c67c93
Wallace143900 <hiperguardi@gmail.com>
2024-05-24 13:22:24 -0300

    alterei os registros na tabela de books
    definindo o author_id como 1 ('Eiichiro Oda')

--- sql/updateData.sql
+++ sql/updateData.sql
@@ -1,3 +1,7 @@
 UPDATE books
 SET name = 'Harry Potter e o Prisioneiro de Azkaban', updated_at = NOW()
 WHERE name = 'Harry Potter';
+
+UPDATE books
+SET author_id = 1, updated_at = NOW()
+WHERE name IN ('One Piece - Volume 1', 'One Piece - Volume 2');
