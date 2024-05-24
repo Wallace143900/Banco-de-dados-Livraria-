07ab1dfe70685fc8e7260568963bd6d58721dc0e
Wallace143900 <hiperguardi@gmail.com>
2024-05-24 13:17:03 -0300

    Esta consulta retorna todos os livros e suas categorias correspondentes
    Ela junta as tabelas books, books_categories e categories, usando as chaves estrangeiras book_id e category_id.

--- sql/selectData.sql
+++ sql/selectData.sql
@@ -5,3 +5,8 @@ FROM books
 JOIN books_categories ON books.id = books_categories.book_id
 JOIN categories ON books_categories.category_id = categories.id
 WHERE categories.name = 'Fantasia';
+
+SELECT books.id AS book_id, books.name AS book_name, categories.id AS category_id, categories.name AS category_name
+FROM books
+JOIN books_categories ON books.id = books_categories.book_id
+JOIN categories ON books_categories.category_id = categories.id;
